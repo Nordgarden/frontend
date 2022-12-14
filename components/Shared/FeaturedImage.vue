1480d7f9432865e08e85c969fecb44499d3f5c94
MIchiel Koning <michiel.koning@valtech.com>
2022-12-14 09:03:29 +0100

    preset

--- components/Shared/FeaturedImage.vue
+++ components/Shared/FeaturedImage.vue
@@ -1,10 +1,9 @@
 <template>
-  <nuxt-picture
+  <img
     alt=""
     :loading="lazy ? 'lazy' : 'eager'"
     :srcset="image.srcSet"
     :src="image.src"
-    :sizes="sizes"
     :width="image.mediaDetails.width"
     :height="image.mediaDetails.height"
     :imgAttrs="{
@@ -14,18 +13,18 @@
 </template>
 
 <script lang="ts" setup>
-  import { IFeaturedImage } from "~~/types/IContent";
+import { IFeaturedImage } from "~~/types/IContent";
 
-  defineProps<{
-    image: IFeaturedImage;
-    sizes: string;
-    lazy?: boolean;
-  }>();
+defineProps<{
+  image: IFeaturedImage;
+  sizes: string;
+  lazy?: boolean;
+}>();
 </script>
 
 <style lang="postcss" scoped>
-  .image {
-    display: block;
-    max-width: 100%;
-  }
+.image {
+  display: block;
+  max-width: 100%;
+}
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,9 @@ export default defineNuxtConfig({
       },
     },
   },
+  nitro: {
+    preset: "netlify-builder",
+  },
   telemetry: false,
   app: {
     head: {
