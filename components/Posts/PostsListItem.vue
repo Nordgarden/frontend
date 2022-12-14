c226d053d20519aa79565fa8398304b6e8b87d64
MIchiel Koning <michiel.koning@valtech.com>
2022-12-14 09:23:47 +0100

    url

--- components/Posts/PostsListItem.vue
+++ components/Posts/PostsListItem.vue
@@ -1,19 +1,26 @@
 <script setup lang="ts">
-  import { IPost } from "~~/types/IContent";
+import { IPost } from "~~/types/IContent";
+const localePath = useLocalePath();
+const props = defineProps<{
+  post: IPost;
+}>();
+const router = useRouter();
+const url = localePath({
+  name: "post",
+  params: {
+    slug: props.post.slug,
+  },
+});
 
-  const props = defineProps<{
-    post: IPost;
-  }>();
-  const router = useRouter();
-  const goToPost = () => {
-    router.push(props.post.slug);
-  };
+const goToPost = () => {
+  router.push(url);
+};
 </script>
 
 <template>
-  <ClickableListItem :to="post.slug" @click="goToPost" class="post">
+  <ClickableListItem @click="goToPost" class="post">
     <h2>
-      <router-link :to="post.slug"><span v-html="post.title" /></router-link>
+      <router-link :to="url"><span v-html="post.title" /></router-link>
     </h2>
     <post-date :date="post.date" class="date" />
     <div class="text" v-html="post.excerpt" />
@@ -33,42 +40,42 @@
 </template>
 
 <style lang="postcss" scoped>
-  .post {
-    display: flex;
-    flex-direction: column;
-    padding-bottom: var(--spacing-m);
-    border-bottom: 2px dashed var(--color-white);
+.post {
+  display: flex;
+  flex-direction: column;
+  padding-bottom: var(--spacing-m);
+  border-bottom: 2px dashed var(--color-white);
 
-    &:hover .read-more,
-    &:focus-within .read-more {
-      box-shadow: 0 3px 0 0 var(--color-primary);
+  &:hover .read-more,
+  &:focus-within .read-more {
+    box-shadow: 0 3px 0 0 var(--color-primary);
 
-      & svg {
-        margin-left: var(--spacing-xxs);
-      }
+    & svg {
+      margin-left: var(--spacing-xxs);
     }
   }
+}
 
-  a {
-    @mixin link-reset;
+a {
+  @mixin link-reset;
+}
+
+.image {
+  order: -2;
+  margin-bottom: var(--spacing-xs);
+
+  &:deep(img) {
+    width: 100%;
+    height: 8em;
+    object-fit: cover;
   }
+}
 
-  .image {
-    order: -2;
-    margin-bottom: var(--spacing-xs);
+.text {
+  @mixin text-overflow;
+}
 
-    &:deep(img) {
-      width: 100%;
-      height: 8em;
-      object-fit: cover;
-    }
-  }
-
-  .text {
-    @mixin text-overflow;
-  }
-
-  .link-wrapper {
-    margin-top: auto;
-  }
+.link-wrapper {
+  margin-top: auto;
+}
 </style>
