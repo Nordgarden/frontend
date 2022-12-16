a2d72750d6a1442c0102a69a3daea8152c1a9a8a
MIchiel Koning <michiel.koning@valtech.com>
2022-12-16 10:48:16 +0100

    eventsApiUrl

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   // toursApiUrl:
   //   "https://rest.bandsintown.com/artists/Nordgarden/events?app_id=TerjeNordgardenWebsite",
-  eventsApiUrl: "https://www.nordgarden.info/.netlify/functions/events",
+  eventsApiUrl: "/.netlify/functions/events",
   pageId: {
     biography: 5,
   },
