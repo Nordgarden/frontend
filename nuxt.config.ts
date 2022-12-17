c42ce632ada8efbeeaa349001b3b8c1a963acbb6
Michiel Koning <mail@michielkoning.nl>
2022-12-17 19:11:52 +0100

    favionc

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,6 +24,34 @@ export default defineNuxtConfig({
           name: "viewport",
           content: "width=device-width,initial-scale=1,viewport-fit=cover",
         },
+
+        {
+          name: "theme-color",
+          content: "#e02830",
+        },
+      ],
+      link: [
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        {
+          rel: "manifest",
+          href: "/site.webmanifest",
+        },
       ],
     },
   },
